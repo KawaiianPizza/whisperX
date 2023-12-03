0a3091a71279c13dbe495e39f7d675eadba92eb8
Kawaiian Pizza <KawaiianPizza@gmail.com>
2023-12-03 15:00:27 -0800

    leaked token :)

--- go.bat
+++ go.bat
@@ -7,6 +7,11 @@ if "%~1"=="" (
     exit /b 1
 )
 
+set "hf_token_file=hf"
+
+REM Read the access token from the file
+set /p hf_token=<"%hf_token_file%"
+
 REM Get the filename from the dragged file
 set "filename=%~1"
 
@@ -17,6 +22,6 @@ REM Change the current directory to the script directory
 cd /d "!scriptdir!"
 echo "!scriptdir!out"   !filename!
 
-whisperx "!filename!" --model large-v2 --align_model WAV2VEC2_ASR_LARGE_LV60K_960H --batch_size 6 --diarize --hf_token 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 --output_format vtt --language en --print_progress True --output_dir out
+whisperx "!filename!" --model large-v2 --align_model WAV2VEC2_ASR_LARGE_LV60K_960H --batch_size 6 --diarize --hf_token !hf_token! --output_format vtt --language en --print_progress True --output_dir out
 py split.py "!scriptdir!out" "!filename!"
 pause
